b816845e533eccbbcfec143632f04de018476740
PantoYT <halasawojciech@gmail.com>
2026-04-12 19:57:33 +0200

    add port to connection.php

--- database/connection.php
+++ database/connection.php
@@ -8,19 +8,20 @@ class Database
     private function __construct()
     {
         $host   = $_ENV['DB_HOST']     ?? 'localhost';
+        $port   = $_ENV['DB_PORT']     ?? '3306';
         $dbname = $_ENV['DB_NAME']     ?? 'tubeyou';
         $user   = $_ENV['DB_USER']     ?? 'root';
         $pass   = $_ENV['DB_PASSWORD'] ?? '';
 
         $this->conn = new PDO(
-            "mysql:host=$host;dbname=$dbname;charset=utf8mb4",
+            "mysql:host=$host;port=$port;dbname=$dbname;charset=utf8mb4",
             $user, $pass
         );
 
         $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         $this->conn->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
     }
-
+    
     public static function getInstance(): Database
     {
         if (self::$instance === null) {
